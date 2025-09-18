f896df6f573d44a71811ab9043e8b7c52c28aa79
Jeevan Prabhath Pendem <pendem.jeevanprabhath+orbc@orbcomm.com>
2025-09-18 18:13:14 +0530

    deploy: publish dist to gh-pages

---
+++ print_dist_css.ps1
@@ -0,0 +1,5 @@
+﻿$a = Get-Content "dist\assets\index-7-MYNkpE.css"
+for($i=860; $i -le 920; $i++){
+  $ln = $i + 1
+  Write-Output ("{0}: {1}" -f $ln, $a[$i])
+}
---
+++ printlines.ps1
@@ -0,0 +1,5 @@
+﻿$lines = Get-Content "src\styles\GlobalStyles.css"
+for ($i=886; $i -le 896; $i++) {
+  $ln = $i + 1
+  Write-Output ("{0}: {1}" -f $ln, $lines[$i])
+}
