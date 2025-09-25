ccff2acc947922613b7ad66230f5db502dc2aab3
Jeevan Prabhath Pendem <pendem.jeevanprabhath+orbc@orbcomm.com>
2025-09-25 21:50:09 +0530

    Restore all user changes and missing files (TopHeader.jsx, index.html, main.jsx, package updates)

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// TODO: Replace with your actual root component
+// import App from './App';
+
+const Root = () => <div>App entry not found. Please add your main component.</div>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
